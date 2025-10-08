66b21f242bafe9ee3f9db00d8d70139ce4007cae
Satabdo Majumder <satabdomajumdertech@gmail.com>
2025-10-08 10:41:24 +0000

    Factory pattern

---
+++ Patterns/Creational/Factory/Bike.java
@@ -0,0 +1,7 @@
+public class Bike implements Transport {
+    @Override
+    public void deliver() {
+        System.out.println("Deliver by Bike");
+    }
+
+}
---
+++ Patterns/Creational/Factory/TransportService.java
@@ -0,0 +1,8 @@
+public class TransportService {
+    public static void main(String args[]){
+        Transport car= new Car();
+        Transport bike =new Bike();
+
+        //if we want to add a new bs then we have to explicity write another clas
+    }    
+}
